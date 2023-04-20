8acb6291cdc7e372ad8f952041c5e9462fae8c2e
Bartłomiej Rudecki <ejbtrd@gmail.com>
2023-04-20 20:13:39 +0200

    revanced-builder: Initial commit

---
+++ patch.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Get latest version of ReVanced tools
+#TOOLS=$(curl -s -X 'GET' 'https://releases.revanced.app/tools' -H 'accept: application/json')
+TOOLS=$(cat tools.json)
+
+TOOLS_NEEDED="revanced-cli revanced-patches revanced-integrations"
+
+for TOOL in ${TOOLS_NEEDED[@]}; do
+    echo "Getting $TOOL"
+    INDEX=""
+    if [ "$TOOL" == "revanced-patches" ]; then
+        INDEX="2"
+    fi
+    TOOL_NAME=$(echo $TOOLS | jq -r ".tools | .[$INDEX] | select(.repository==\"revanced/$TOOL\") | .name")
+    TOOL_URL=$(echo $TOOLS | jq -r ".tools | .[$INDEX] | select(.repository==\"revanced/$TOOL\") | .browser_download_url")
+
+    if [ -f "$TOOL_NAME" ]; then
+        echo "$TOOL_NAME already downloaded, skipping!"
+        continue
+    fi
+
+    wget -q "$TOOL_URL" -O "$TOOL_NAME"
+done
+
+function get_name() {
+    INDEX=""
+    if [ "$1" == "revanced-patches" ]; then
+        INDEX="2"
+    fi
+    echo $TOOLS | jq -r ".tools | .[$INDEX] | select(.repository==\"revanced/$1\") | .name"
+}
+
+REVANCED_CLI=$(get_name "revanced-cli")
+REVANCED_PATCHES=$(get_name "revanced-patches")
+REVANCED_INTEGRATIONS=$(get_name "revanced-integrations")
+
+# Get patches
+wget -q https://releases.revanced.app/patches -O patches.json
+
+# Get latest version of apkkeep
+echo "Getting apkkeep"
+
+APKKEEP_URL=$(curl -s -X GET https://api.github.com/repos/EFForg/apkeep/releases/latest | jq -r ".assets[] | select(.name | contains(\"apkeep-x86_64-unknown-linux-gnu\") and (contains(\".sig\") | not)) | .browser_download_url")
+if [ ! -f "apkkeep" ]; then
+    wget -q "$APKKEEP_URL" -O apkkeep
+    chmod +x apkkeep
+fi
+
+# Get latest youtube version
+YOUTUBE_VERSION=$(cat patches.json | jq -r ".[] | select(.name==\"spoof-app-version\") | .compatiblePackages | .[] | .versions | .[-1]")
+
+# Download YouTube apk
+YOUTUBE_APK="com.google.android.youtube@$YOUTUBE_VERSION"
+./apkkeep -a "$YOUTUBE_APK" .
+
+# Finally patch the apk
+java -jar "$REVANCED_CLI" \
+  -a "$YOUTUBE_APK".apk \
+  -b "$REVANCED_PATCHES" \
+  -m "$REVANCED_INTEGRATIONS" \
+  -o revanced_youtube_"$YOUTUBE_VERSION".apk
