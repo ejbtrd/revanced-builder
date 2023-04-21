966e44c6aa3fa0bc7d76d778ee30b17bebf682c8
Bartłomiej Rudecki <ejbtrd@gmail.com>
2023-04-21 23:44:27 +0200

    revanced-builder: Show latest patched and revanced youtube version

--- check_yt_version.sh
+++ check_yt_version.sh
@@ -4,7 +4,10 @@ LOCAL_YOUTUBE_VERSION=$(cat yt-version)
 
 REVANCED_YOUTUBE_VERSION=$(curl -s -X GET https://releases.revanced.app/patches | jq -r ".[] | select(.name==\"spoof-app-version\") | .compatiblePackages | .[] | .versions | .[-1]")
 
-if [ "$LOCAL_YOUTUBE_VERSION" == "$REVANCED_YOUTUBE_VERSION" ]; then
+echo "Latest patched YouTube: $LOCAL_YOUTUBE_VERSION"
+echo "Latest supported by ReVanced: $REVANCED_YOUTUBE_VERSION"
+
+if [ "$LOCAL_YOUTUBE_VERSION" = "$REVANCED_YOUTUBE_VERSION" ]; then
     echo "Latest YouTube version supported by ReVanced is equal to last patched version, aborting!"
     exit 1
 fi
